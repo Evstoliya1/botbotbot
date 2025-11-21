a9e6560d97a9d40436aa3e4bd348ab0588869b8c
Evstoliya1 <evstoliya.shusharina@gmail.com>
2025-11-21 18:33:16 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+token = '8595425043:AAG6an_fTKhw19okCfKh60951wkDxr9e28s'
+database = 'database'
---
+++ logic.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+buttons = [ (_,) for _ in (['my_schedule', 'all_schedule'])]
+
+classes = ['5', '6', '7', '8', '9', '10', '11']
